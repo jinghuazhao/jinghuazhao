50d3d2d5ef6936ae4568fe312f5f5412d1ed9d81
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2022-11-13 12:44:59 +0000

    st.sh

--- st.sh
+++ st.sh
@@ -2,7 +2,7 @@
 
 git add POST.sh
 git commit -m "POST.sh"
-git add README.md gansubaiyin.svg
+git add README.md gansubaiyin.svg jhz-50.png
 git commit -m "README.md"
 git add st.sh
 git commit -m "st.sh"
