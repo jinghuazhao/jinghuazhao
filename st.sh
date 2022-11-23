cfe2f16922e2a8dbb607c3b1b2f461f02197451a
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2022-11-23 15:12:32 +0000

    st.sh

--- st.sh
+++ st.sh
@@ -2,7 +2,7 @@
 
 git add POST.sh
 git commit -m "POST.sh"
-git add README.md gansubaiyin.svg gansubaiyin-circle.png jhz-50.png
+git add README.md gansubaiyin.svg gansubaiyin-circle.png jhz-50.png CASP14-t1091.gif
 git commit -m "README.md"
 git add st.sh
 git commit -m "st.sh"
