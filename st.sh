4cecbf38ee8855e9adc31d3a3fefeed8056c0e19
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2024-09-27 11:23:22 +0100

    st.sh

--- st.sh
+++ st.sh
@@ -2,7 +2,7 @@
 
 git add POST.sh
 git commit -m "POST.sh"
-git add README.md gansubaiyin.svg gansubaiyin-circle.png CASP14-t1091-text.gif mermaid-diagram-2024-07-20-224238.svg
+git add README.md gansubaiyin.svg gansubaiyin-circle.png CASP14-t1091-text.gif
 git commit -m "README.md"
 git add st.sh
 git commit -m "st.sh"
